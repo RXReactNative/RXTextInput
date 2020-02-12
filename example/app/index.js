6c5f75498ac96d063e83b7ae58cc4357cac07a35
srxboys <srxboys@126.com>
2020-02-12 14:37:23 +0800

    update docs

--- example/app/index.js
+++ example/app/index.js
@@ -37,67 +37,96 @@ export default class RXInputDemo extends Component {
         <View style={styles.contentView}>
           <RXInput
             style={{fontSize: 14, color: 'red', backgroundColor: 'white'}}
-            placeholder={'输入详细地址'}
-            returnKeyType={'done'}
+            placeholder={'type=text'}
           />
-
           <View style={styles.line} />
-
           <RXInput
-            maxLength={20}
             style={{fontSize: 14, color: 'red', backgroundColor: 'white', textAlign: 'right'}}
-            placeholder={'输入详细地址 + {textAlign: "right"}'}
-            returnKeyType={'done'}
+            placeholder={'type=text style="textAlign: right"'}
           />
           <View style={styles.line} />
-
           <RXInput
+            type='number'
             maxLength={20}
+            style={{fontSize: 14, color: 'red', backgroundColor: 'white'}}
+            placeholder={'type="number" 数字键盘'}
+          />
+          <View style={styles.line} />
+          <RXInput
+            type='int'
             style={{margin:5, height: 30, borderWidth: 1, borderColor: '#9400D3', paddingLeft: 5, paddingRight: 5, borderRadius: 3}}
-            placeholder={'输入的 序列号，作为 Alias的唯一标识'}
+            placeholder={'type=int "数字键盘 - 整型"'}
             placeholderColor={'#8B8B7A'}
             placeholderFontSize={12}
-            returnKeyType={'done'}
           />
           <View style={styles.line} />
-
           <RXInput
-            type={'idCardNo'}
+            type='float'
+            placeholder={'type=float "数字键盘 - 浮点型(最多保留2位小数)"'}
             style={{margin:5, height: 30, borderWidth: 1, borderColor: '#9400D3', paddingLeft: 5, paddingRight: 5, borderRadius: 3}}
-            placeholder={'请输入身份证号，字母需大写'}
-            returnKeyType={'done'}
           />
           <View style={styles.line} />
-
           <RXInput
             type='phone'
             style={{height: 30}}
             placeholder={'请输入手机号'}
           />
           <View style={styles.line} />
-
           <RXInput
             type='VIN'
             style={{height: 30}}
             placeholder={'请输入车架号, 字母在键盘消失后 自动大写'}
           />
           <View style={styles.line} />
+          <RXInput
+            type={'password'}
+            style={{margin:5, height: 30, borderWidth: 1, borderColor: '#9400D3', paddingLeft: 5, paddingRight: 5, borderRadius: 3}}
+            placeholder={'请输入密码'}
+            returnKeyType={'done'}
+          />
           <View style={styles.line} />
-
+          <View style={styles.line} />
+          <Text style={styles.tip}>{"下面验证需要处理"}</Text>
+          <RXInput
+            type={'idCardNo'}
+            style={{margin:5, height: 30, borderWidth: 1, borderColor: '#9400D3', paddingLeft: 5, paddingRight: 5, borderRadius: 3}}
+            placeholder={'请输入身份证号码, 字母在键盘消失后 `不能`自动大写'}
+            returnKeyType={'done'}
+          />
+          <View style={styles.line} />
+          <RXInput
+            type={'captcha'}
+            style={{margin:5, height: 30, borderWidth: 1, borderColor: '#9400D3', paddingLeft: 5, paddingRight: 5, borderRadius: 3}}
+            placeholder={'请输入验证码 `字母+数字`'}
+            returnKeyType={'done'}
+          />
+          <View style={styles.line} />
+          <RXInput
+            type={'graphCaptcha'}
+            style={{margin:5, height: 30, borderWidth: 1, borderColor: '#9400D3', paddingLeft: 5, paddingRight: 5, borderRadius: 3}}
+            placeholder={'请输入校验图形验证码 `字母+数字`'}
+            returnKeyType={'done'}
+          />
+          <View style={styles.line} />
+          <RXInput
+            type={'email'}
+            style={{margin:5, height: 30, borderWidth: 1, borderColor: '#9400D3', paddingLeft: 5, paddingRight: 5, borderRadius: 3}}
+            placeholder={'请输入email，字母需大写 `格式不对`'}
+            returnKeyType={'done'}
+          />
+          <View style={styles.line} />
           <RXInput
             type='bankCard'
             style={{height: 30}}
-            placeholder={'请输入银行卡号, 有问题'}
+            placeholder={'请输入银行卡号, `自动加空格，位数`'}
           />
           <View style={styles.line} />
-
           <RXInput
             type='amount'
             style={{height: 30}}
-            placeholder={'请输入金额, 有问题'}
+            placeholder={'请输入金额, `位数`'}
           />
           <View style={styles.line} />
-          
         </View>
       </View>
     )
